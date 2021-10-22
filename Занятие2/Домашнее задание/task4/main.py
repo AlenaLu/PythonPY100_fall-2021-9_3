9b8b57b28aa3e69e9868eac804bd5ca8835a648d
AlenaLu <pinkjoy18@gmail.com>
2021-10-23 03:35:26 +0500

    init

--- Занятие2/Домашнее задание/task4/main.py
+++ Занятие2/Домашнее задание/task4/main.py
@@ -1,8 +1,8 @@
-if __name__ == "__main__":
-   list_ = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#if __name__ == "__main__":
+list_ = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-   list_[0] = list_[10]
-   list_[10] = list_[0]
-    # Write your solution here
+list_[0], list_[-1] = list_[-1],list_[0]
+
+    #Write your solution here
     #pass
 print(list_)
