c9b8deb5c3b36f24845cce4b421bae0afce92b71
AlenaLu <pinkjoy18@gmail.com>
2021-10-23 03:40:28 +0500

    init

--- Занятие2/Домашнее задание/task3/main.py
+++ Занятие2/Домашнее задание/task3/main.py
@@ -1,10 +1,10 @@
-A = input("введите число А:" )
-B = input("введите число А:" )
-С = input("введите число А:" )
+A = int(input("введите число А:" ))
+B = int(input("введите число А:" ))
+C = int(input("введите число А:" ))
 
 cond_1 = A < 45
 cond_2 = B < 45
 cond_3 = C < 45
 
 if cond_1 or cond_2 or cond_3:
-    print()
+    print('+')
