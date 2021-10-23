f7ea53e26ab249a71b3667e562d7df5bd5762dc8
AlenaLu <pinkjoy18@gmail.com>
2021-10-23 12:37:17 +0500

    init

--- Занятие3/Практические_задания/task1_3/main.py
+++ Занятие3/Практические_задания/task1_3/main.py
@@ -8,14 +8,14 @@ def incorrect_func(name_arg=[]):
 def incorrect_main():
     # вызовем два раза одну и ту же функцию
     incorrect_func()
-    print('-----')
+    print('-----')  #просто для визуального разделения
     incorrect_func()
-
+# вот так лучше не делать, функция получается нечистая
 
 # установим аргумент name_arg пустым а внутри функции будем проверять его
 def correct_func(name_arg=None):
-    if name_arg is None:
-        name_arg = []
+    if name_arg is None:  # id(name_arg) ==id(None)
+        name_arg = []  #cоздание списка внутри тела
     print("Аргумент до изменения", name_arg)
     name_arg.append(1)
     print("Аргумент после изменения", name_arg)
@@ -33,5 +33,5 @@ def correct_main():
 
 
 if __name__ == "__main__":
-    incorrect_main()
+    #incorrect_main()
     correct_main()
--- Занятие3/Практические_задания/task1_4/main.py
+++ Занятие3/Практические_задания/task1_4/main.py
@@ -1,10 +1,10 @@
-PI = 3.14
+PI = 3.14  #constant
 
 
-def square_circle(r):
-    return PI * r ** 2
+def square_circle(r):  #функция которая будет искать площадь круга
+    return PI * r ** 2   #функция ищет r в локальной области видимости
 
 
 if __name__ == "__main__":
-    square = square_circle(5)
+    square = square_circle(5)  #r=5 и подставляется в строку 5
     print(square)
